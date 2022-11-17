40fcf1d83795dc58b50bd644b1b1d31bf9a0400a
Prarthana <69465478+prarthanachandak@users.noreply.github.com>
2022-11-17 07:41:43 +0530

    0-1 knapsack

--- 4.cpp
+++ 4.cpp
@@ -52,3 +52,10 @@ int main()
 // where ‘N’ is the number of weight element and ‘W’ is capacity. As for every weight element we traverse through all weight capacities 1<=w<=W.
 // Auxiliary Space: O(N*W). 
 // The use of 2-D array of size ‘N*W’.
+
+// In the Dynamic programming we will work considering the same cases as mentioned in the recursive approach. In a DP[][] table let’s consider all the possible weights from ‘1’ to ‘W’ as the columns and weights that can be kept as the rows. 
+// The state DP[i][j] will denote maximum value of ‘j-weight’ considering all values from ‘1 to ith’. So if we consider ‘wi’ (weight in ‘ith’ row) we can fill it in all columns which have ‘weight values > wi’. Now two possibilities can take place: 
+
+// Fill ‘wi’ in the given column.
+// Do not fill ‘wi’ in the given column.
+// Now we have to take a maximum of these two possibilities, formally if we do not fill ‘ith’ weight in ‘jth’ column then DP[i][j] state will be same as DP[i-1][j] but if we fill the weight, DP[i][j] will be equal to the value of ‘wi’+ value of the column weighing ‘j-wi’ in the previous row. So we take the maximum of these two possibilities to fill the current state. This visualisation will make the concept clear:  
